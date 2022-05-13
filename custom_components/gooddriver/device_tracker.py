9639827649f6ca96dcf8288c574bdae6e7eabd36
dscao <dscao.com@gmail.com>
2022-05-14 01:31:58 +0800

    Update device_tracker.py

--- custom_components/gooddriver/device_tracker.py
+++ custom_components/gooddriver/device_tracker.py
@@ -1,7 +1,6 @@
 """Support for the gooddriver service."""
 import logging
 import time, datetime
-from datetime import datetime, timedelta
 
 from homeassistant.components.device_tracker.config_entry import TrackerEntity
 from homeassistant.helpers.device_registry import DeviceEntryType
@@ -38,24 +37,17 @@ from .const import (
 )
 
 
-
-
 PARALLEL_UPDATES = 1
 _LOGGER = logging.getLogger(__name__)
 
-laststoptime = "未知"
-lastlat = "未知"
-lastlon = "未知"
-runorstop = "未知"
-thislat = "未知"
-thislon = "未知"
+
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Add gooddriver entities from a config_entry."""
     name = config_entry.data[CONF_NAME]    
     attr_show = config_entry.options.get(CONF_ATTR_SHOW, True)
     coordinator = hass.data[DOMAIN][config_entry.entry_id][COORDINATOR]
-    _LOGGER.debug("user_id: %s ,coordinator 0 is ok: %s", name, coordinator.data["ERROR_CODE"])
+    _LOGGER.debug("user_id: %s ,coordinator result: %s", name, coordinator.data)
 
     async_add_entities([gooddriverEntity(name, attr_show, coordinator)], False)
 
@@ -66,12 +58,10 @@ class gooddriverEntity(TrackerEntity):
     def __init__(self, name, attr_show, coordinator):
         
         self.coordinator = coordinator
-        _LOGGER.debug("coordinator HD_STATE_TIME: %s", coordinator.data["MESSAGE"]["HD_STATE_TIME"])
+        _LOGGER.debug("coordinator: %s", coordinator.data)
         self._name = name
         self._attrs = {}
-        self._attr_show = attr_show
-        
-        
+        self._attr_show = attr_show       
 
     @property
     def name(self):            
@@ -112,112 +102,34 @@ class gooddriverEntity(TrackerEntity):
     def source_type(self):
         return "GPS"
 
-    # @property
-    # def is_connected(self):
-        # return True if self.coordinator.data["ERROR_CODE"]==0 else False
-
     @property
-    def latitude(self):
-        
-        global laststoptime
-        global lastlat
-        global lastlon
-        global thislat
-        global thislon
-        global runorstop
-
-        data = self.coordinator.data.get("MESSAGE")
-        _LOGGER.debug("latitude result data: %s", data)
-        if data:            
-            self._querytime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            self._updatetime = data["HD_RECENT_LOCATION"]["Time"]
-            self._speed = data["HD_RECENT_LOCATION"]["Speed"]
-            self._course = data["HD_RECENT_LOCATION"]["Course"]
-            thislat = data["HD_RECENT_LOCATION"]["Lat"]
-            thislon = data["HD_RECENT_LOCATION"]["Lng"]
-            if data["HD_STATE"] == 1:
-                self._status = "车辆点火"
-            elif data["HD_STATE"] == 2:
-                self._status = "车辆熄火"
-            else:
-                self._status = "未知"
-                
-            if thislat == lastlat and thislon == lastlon and runorstop == "运动":
-                laststoptime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                runorstop = "静止"
-            elif thislat != lastlat or thislon != lastlon:
-                lastlat = data["HD_RECENT_LOCATION"]["Lat"]
-                lastlon = data["HD_RECENT_LOCATION"]["Lng"]
-                runorstop = "运动"  
-                
-            def time_diff (timestamp):
-                result = datetime.now() - datetime.fromtimestamp(timestamp)
-                hours = int(result.seconds / 3600)
-                minutes = int(result.seconds % 3600 / 60)
-                seconds = result.seconds%3600%60
-                if result.days > 0:
-                    return("{0}天{1}小时{2}分钟".format(result.days,hours,minutes))
-                elif hours > 0:
-                    return("{0}小时{1}分钟".format(hours,minutes))
-                elif minutes > 0:
-                    return("{0}分钟{1}秒".format(minutes,seconds))
-                else:
-                    return("{0}秒".format(seconds))     
-            self._parking_time=time_diff(int(time.mktime(time.strptime(data["HD_RECENT_LOCATION"]["Time"], "%Y-%m-%d %H:%M:%S")))) 
-            
-        return (float(thislat) + 0.00240)
+    def latitude(self):                
+        return (float(self.coordinator.data["thislat"]) + 0.00240)
 
     @property
     def longitude(self):
-        return (float(thislon) - 0.00540)
+        return (float(self.coordinator.data["thislon"]) - 0.00540)
         
     @property
     def location_accuracy(self):
-        return 10
-        
-    # @property
-    # def battery_level(self):
-        # return 100
-    #if (MAJOR_VERSION, MINOR_VERSION) < (2022, 4):
+        return 10        
+
     @property
     def state_attributes(self): 
         attrs = super(gooddriverEntity, self).state_attributes
-        data = self.coordinator.data.get("MESSAGE")
-        if data:
-            attrs[ATTR_SPEED] = self._speed
-            attrs[ATTR_COURSE] = self._course
-            
+        #data = self.coordinator.data.get("result")
+        data = self.coordinator.data
+        if data:             
+            attrs["speed"] = data["speed"]
+            attrs["speed"] = data["speed"]
+            attrs[ATTR_STATUS] = data["status"]
+            attrs["updatetime"] = data["updatetime"]        
+            attrs[ATTR_QUERYTIME] = data["querytime"]
             if self._attr_show == True:
-                attrs[ATTR_STATUS] = self._status
-                attrs[ATTR_RUNORSTOP] = runorstop        
-                attrs[ATTR_LASTSTOPTIME] = laststoptime
-                attrs[ATTR_UPDATE_TIME] = self._updatetime        
-                attrs[ATTR_QUERYTIME] = self._querytime
-                attrs[ATTR_PARKING_TIME] = self._parking_time
-            
-        return attrs
-    # else:
-        # @property
-        # def extra_state_attributes(self):
-            # """Return the state attributes."""
-            # attrs = super().extra_state_attributes
-            # _LOGGER.debug("2022.4 later attrs: %s", attrs)            
-            
-            # data = self.coordinator.data.get("MESSAGE")
-            # if data:
-                # attrs[ATTR_SPEED] = self._speed
-                # attrs[ATTR_COURSE] = self._course
-                
-                # if self._attr_show == True:
-                    # attrs[ATTR_STATUS] = self._status
-                    # attrs[ATTR_RUNORSTOP] = runorstop        
-                    # attrs[ATTR_LASTSTOPTIME] = laststoptime
-                    # attrs[ATTR_UPDATE_TIME] = self._updatetime        
-                    # attrs[ATTR_QUERYTIME] = self._querytime
-                    # attrs[ATTR_PARKING_TIME] = self._parking_time
-                
-            # return attrs
-
+                attrs[ATTR_RUNORSTOP] = data["runorstop"]
+                attrs[ATTR_LASTSTOPTIME] = data["laststoptime"]
+                attrs[ATTR_PARKING_TIME] = data["parkingtime"]  
+        return attrs 
 
     async def async_added_to_hass(self):
         """Connect to dispatcher listening for entity data notifications."""
